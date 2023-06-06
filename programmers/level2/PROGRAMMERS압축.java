c07e9c1ba6080d5bb79c143c0ed99dd386f5e59f
Wonyoung Lee <74983448+210-reverof@users.noreply.github.com>
2023-06-06 23:49:39 +0900

    feat: PROGRAMMERS 압축 해결

---
+++ programmers/level2/PROGRAMMERS압축.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> solution(String msg) {
+        List<Integer> ans = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
+        int idx = 27;
+        
+        for (int i = 0; i < 26; i++)
+            map.put((char)('A' + i) + "", i+1);
+        
+        int start = 0, end = 2;
+        while(end <= msg.length()) {
+            String curr = msg.substring(start, end);
+            
+            if (map.containsKey(curr)) {
+                end++;
+                continue;
+            }
+            
+            ans.add(map.get(msg.substring(start, end-1)));
+            map.put(curr, idx++);
+            start += (end - start - 1);
+            end++;
+        }
+        
+        return ans;
+    }
+}
