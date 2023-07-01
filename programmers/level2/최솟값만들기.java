107ec2a54d389f062012ba911002e76182f80476
Wonyoung Lee <74983448+210-reverof@users.noreply.github.com>
2023-07-01 23:05:42 +0900

    feat: programmers 최솟값만들기 해결

---
+++ programmers/level2/최솟값만들기.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution
+{
+    public int solution(int []A, int []B)
+    {
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int size = A.length;
+        
+        int answer = 0;
+        for (int i = 0; i < size; i++)
+            answer += A[i] * B[size - i - 1]; 
+
+        
+        return answer;
+    }
+}
