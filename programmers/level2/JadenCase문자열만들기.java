6f1f04614c362a574ab7008bf1febeb4d44ed01f
Wonyoung Lee <74983448+210-reverof@users.noreply.github.com>
2023-07-04 23:39:26 +0900

    feat: programmers JadenCase 문자열 만들기 해결

--- programmers/level2/JadenCase문자열만들기.java
+++ programmers/level2/JadenCase문자열만들기.java
@@ -1,19 +1,16 @@
 class Solution {
     public String solution(String s) {
         String answer = "";
+        
         StringBuffer sb = new StringBuffer();
-        
-        String[] temp = s.toLowerCase().split(" ");
-        
-        for(int i = 0; i < temp.length; i++){
-            if(temp[i].length() == 1)
-                sb.append(Character.toUpperCase(temp[i].charAt(0)));
-            else{
-            sb.append(Character.toUpperCase(temp[i].charAt(0)) 
-                     + temp[i].substring(1));
-            }
-            if(i != temp.length - 1) sb.append(" ");
-        } 
+        s = s.toLowerCase();
+        sb.append(Character.toUpperCase(s.charAt(0)));
+        for(int i = 1; i < s.length(); i++){
+            if(s.charAt(i) == ' ') sb.append(" ");
+            else if(s.charAt(i - 1) == ' ')
+                sb.append(Character.toUpperCase(s.charAt(i)));
+            else sb.append(s.charAt(i));
+        }
         return sb.toString();
     }
 }
