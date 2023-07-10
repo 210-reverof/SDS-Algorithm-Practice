910339801ae0c8f3f58ec36ee0ee04b3ec7c8442
Wonyoung Lee <74983448+210-reverof@users.noreply.github.com>
2023-07-10 23:35:05 +0900

    feat: programmers 피보나치 수 완료

---
+++ programmers/level2/피보나치수.java
@@ -0,0 +1,16 @@
+class Solution {
+    int[] fibo;
+
+    public int solution(int n) {
+        fibo = new int[n+1];
+        return dfs(n);
+    }
+
+    private int dfs(int n) {
+        if(fibo[n] > 0) return fibo[n];
+        if(n == 0) return 0;
+        if(n == 1) return fibo[1] = 1;
+
+        return fibo[n] = (dfs(n-1) + dfs(n-2))%1234567;
+    }
+}
