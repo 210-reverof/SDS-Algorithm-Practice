4b03cc496885822a78b1b532308bef31b82701d1
Wonyoung Lee <74983448+210-reverof@users.noreply.github.com>
2023-10-20 14:15:59 +0000

    feat: programmer 숫자 게임 재풀이 완료

--- programmers/level3/숫자게임.java
+++ programmers/level3/숫자게임.java
@@ -4,31 +4,22 @@ class Solution {
     int[] a, b;
     boolean[] used;
     int size;
+    int answer = 0;
     public int solution(int[] A, int[] B) {
-        int answer = 0;
+        Arrays.sort(A);
         Arrays.sort(B);
-        a = A; 
-        b = B; 
         size = A.length;
-        used = new boolean[size];
+        boolean[] used = new boolean[size];
         
         for (int i = 0; i < size; i++) {
-             getWinIdx(a[i]);
+             for (int j = i; j < size; j++) {
+                if (used[j] || B[j] <= A[i]) continue;
+                used[j] = true;
+                answer++;
+                break;
+            }
         }
         
         return answer;
-    }
-    
-    private int getWinIdx(int num) {
-        if (num >= b[size - 1]) return -1;
-        int left = 0, right = size - 1;
-        
-        while (left <= right) {
-            int mid = (right + left) / 2;
-            if (num == b[mid]) break;
-            left++;
-        }
-        
-        return mid + 1;
     }
 }
