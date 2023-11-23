00ac9bcff36154f9387bef1ba99f3b271688ed3e
Wonyoung Lee <74983448+210-reverof@users.noreply.github.com>
2023-11-23 16:41:11 +0900

    refactor: programmers 파괴되지 않은 건물 해결

--- programmers/level3/파괴되지않은건물.java
+++ programmers/level3/파괴되지않은건물.java
@@ -1,27 +1,31 @@
 import java.util.*;
 
 class Solution {
-    int[][] b;
-    int answer;
     public int solution(int[][] board, int[][] skill) {
-        b = board;
-        answer = board.length * board[0].length;
+        int[][] points = new int[board.length + 1][board[0].length + 1];
+        int[][] sums = new int[board.length][board[0].length];
+        int H = board.length, W = board[0].length;
+        int answer = 0;
         
         for (int[] s : skill) {
-            if (s[0] == 1) affect(s[1], s[2], s[3], s[4], -1 * s[5]);
-            else affect(s[1], s[2], s[3], s[4], s[5]);
+            int e = s[0] == 1? -1 * s[5] : s[5];
+            points[s[1]][s[2]] += e;
+            points[s[1]][s[4]+1] += -1 * e;
+            points[s[3]+1][s[2]] += -1 * e;
+            points[s[3]+1][s[4]+1] += e;
+        }
+        
+        for (int i = 0; i < H; i++) {
+            for (int j = 0; j < W; j++) {
+                int leftTop = i > 0 && j > 0? sums[i-1][j-1] : 0;
+                int top = i > 0? sums[i-1][j] : 0;
+                int left = j > 0? sums[i][j-1] : 0;
+                
+                sums[i][j] = top + left - leftTop + points[i][j];
+                if (sums[i][j] + board[i][j] > 0) answer++;
+            }
         }
         
         return answer;
-    }
-    
-    private void affect(int r1, int c1, int r2, int c2, int degree) {
-        for (int i = r1; i <= r2; i++) {
-            for (int j = c1; j <= c2; j++) {
-                if (b[i][j] > 0 && b[i][j] + degree <= 0) answer--;
-                else if (b[i][j] <= 0 && b[i][j] + degree > 0) answer++;
-                b[i][j] += degree;
-            }
-        }
     }
 }
