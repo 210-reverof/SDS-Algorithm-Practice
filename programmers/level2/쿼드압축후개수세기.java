64bc3512e0157558fc9d53923912ad62392498ce
Wonyoung Lee <74983448+210-reverof@users.noreply.github.com>
2023-11-29 15:43:35 +0900

    refactor: programmers 쿼드 압축 후 개수 세기 해결

--- programmers/level2/쿼드압축후개수세기.java
+++ programmers/level2/쿼드압축후개수세기.java
@@ -1,33 +1,38 @@
-class Solution {
-    static int[] answer;
-    public int[] solution(int[][] arr) {
-        answer = new int[2];
+import java.util.*;
 
-        recursion(arr, 0, 0, arr.length);
-        return answer;
+class Solution {
+    int[][] board;
+    public int[] solution(int[][] arr) {
+        board = arr;
+        int[] answer = find(0, 0, arr.length);
+        
+        return new int[]{answer[0], answer[1]};
     }
     
-    void recursion(int[][] arr, int x, int y, int size) {
-        if (compact(arr, x, y, size)) {
-            answer[arr[x][y]]++;
-            return;
+    // { 0개수, 1개수, allSame }
+    private int[] find(int startR, int startC, int n) {
+        if (n == 1) {
+            return board[startR][startC] == 0? new int[]{1, 0, 1} : new int[]{0, 1, 1};
         }
         
-        recursion(arr, x, y, size / 2);
-        recursion(arr, x + size / 2, y, size / 2);
-        recursion(arr, x, y + size / 2, size / 2);
-        recursion(arr, x + size / 2, y + size / 2, size / 2);
-    }
-
-    static boolean compact(int[][] arr, int x, int y, int size) {
-        for (int i = x; i < x + size; i++) {
-            for (int j = y; j < y + size; j++) {
-                // 영역의 첫번째 값과 나머지를 비교하다 하나다로 다르면 반환
-                if(arr[x][y] != arr[i][j]) {
-                    return false;
-                }
-            }
+        int[] a = find(startR, startC, n/2);
+        int[] b = find(startR, startC + n/2, n/2);
+        int[] c = find(startR + n/2, startC, n/2);
+        int[] d = find(startR + n/2, startC + n/2, n/2);
+        
+        int cnt0 = a[0] + b[0] + c[0] + d[0];
+        int cnt1 = a[1] + b[1] + c[1] + d[1];
+        
+        if (cnt0 == 0) {
+            cnt1 = 1;
+            return new int[]{cnt0, cnt1, 1};   
         }
-        return true;
+        
+        if (cnt1 == 0) {
+            cnt0 = 1;
+            return new int[]{cnt0, cnt1, 1};
+        }
+        
+        return new int[]{cnt0, cnt1, 0}; 
     }
 }
