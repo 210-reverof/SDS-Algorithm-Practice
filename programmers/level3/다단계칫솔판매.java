e81674a690d3b922da3a419c5a0916c8340d3763
Wonyoung Lee <74983448+210-reverof@users.noreply.github.com>
2024-04-23 11:23:08 +0900

    [프로그래머스] 다단계 칫솔 판매 해결

--- programmers/level3/다단계칫솔판매.java
+++ programmers/level3/다단계칫솔판매.java
@@ -1,23 +1,19 @@
 import java.util.*;
 
 class Solution {
-    Map<String, List<String>> tree = new HashMap<>();
+    Map<String, String> parents = new HashMap<>();
     Map<String, Integer> money = new HashMap<>();
     public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
         int N = enroll.length;
         int[] answer = new int[N];
-        tree.put("-", new LinkedList<>());
         
         for (int i = 0; i < N; i++) {
-            tree.put(enroll[i], new LinkedList<>());
-            tree.get(referral[i]).add(enroll[i]);
+            parents.put(enroll[i], referral[i]);
         }
         
         for (int i = 0; i < seller.length; i++) {
-            money.put(seller[i], amount[i] * 100);
+            sell(seller[i], amount[i] * 100);
         }
-        
-        dfs("-");
         
         for (int i = 0; i < N; i++) {
             answer[i] = money.getOrDefault(enroll[i], 0);
@@ -26,18 +22,14 @@ class Solution {
         return answer;
     }
     
-    private int dfs(String key) {
-        List<String> childs = tree.get(key);
-        int sum = money.getOrDefault(key, 0);
+    private void sell(String name, int count) {
+        String parent = parents.get(name);
         
-        int giveSum = (int)(sum * 0.1);
-        for (String child: childs) {
-            int currSum = dfs(child);
-            sum += currSum;
-            giveSum += (int)(currSum * 0.1);
+        int giveMoney = (int)(count * 0.1);
+        money.put(name, money.getOrDefault(name, 0) + (count - giveMoney));
+        
+        if (giveMoney >= 1) {
+            sell(parent, giveMoney);
         }
-        money.put(key, sum - giveSum);
-        
-        return giveSum;
     }
 }
