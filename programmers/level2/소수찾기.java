ea5d6d5fd26b88b0f377f5ba37272910ec28810f
Wonyoung Lee <74983448+210-reverof@users.noreply.github.com>
2024-05-09 10:42:41 +0900

    [프로그래머스] 소수 찾기 해결

--- programmers/level2/소수찾기.java
+++ programmers/level2/소수찾기.java
@@ -1,23 +1,55 @@
+import java.util.*;
+
 class Solution {
-    public int solution(int n) {
-        int answer = 0;
+    char[] digits;
+    int size = 0, answer = 0;
+    Set<Integer> nums = new HashSet<>();
+    public int solution(String numbers) {
+        digits = numbers.toCharArray();        
+        size = digits.length;
         
-        int[] number = new int[n+1];
-        
-        for(int i = 0; i < n+1; i++) number[i] = i;
-        
-        for(int i = 2; i < n+1; i++) {
-            if(number[i] != 0) {
-                for(int j=i+i; j<n+1; j+=i) {
-                    number[j] = 0;
-                }
-            }
-        }
-        
-        for(int i = 2; i < n+1; i++) {
-            if(number[i] != 0) answer++;
+        for (int i = 1; i <= size; i++) {
+            dfs(i, new boolean[size], new int[i], 0);
         }
         
         return answer;
+    }
+    
+    private void dfs(int targetLen, boolean[] used, int[] sel, int depth) {
+        if (depth == targetLen) {
+            int num = getNum(targetLen, sel);
+            if (isPrime(num) && !nums.contains(num)) {
+                System.out.println();
+                answer++;
+                nums.add(num);
+            }
+            return;
+        }
+        
+        for (int i = 0; i < size; i++) {
+            if (used[i]) continue;
+            used[i] = true;
+            sel[depth] = i;
+            dfs(targetLen, used, sel, depth+1);
+            used[i] = false;
+        }
+    }
+    
+    private int getNum(int targetLen, int[] sel) {
+        String str = "";
+        for (int s: sel) {
+            str += digits[s];
+        }
+        return Integer.parseInt(str);
+    }
+    
+    private boolean isPrime(int n) {
+        if (n == 0 || n == 1) return false;
+        for (int i = 2; i<=(int) Math.sqrt(n); i++) {
+          if (n % i == 0) {
+              return false;
+          }
+        }
+        return true;
     }
 }
