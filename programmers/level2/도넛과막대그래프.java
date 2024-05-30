3e631d01a0c8ca23a2d3d2c4e3262f03a03dd546
Wonyoung Lee <74983448+210-reverof@users.noreply.github.com>
2024-05-30 11:33:06 +0900

    [프로그래머스] 도넛과 막대 그래프 해결

--- programmers/level2/도넛과막대그래프.java
+++ programmers/level2/도넛과막대그래프.java
@@ -1,127 +1,38 @@
 import java.util.*;
 
 class Solution {
-    Map<Integer, List<Integer>> graph = new HashMap<>();
-    Set<Integer> vs = new HashSet<>();
-    Queue<Integer> q = new LinkedList<>();
-    Map<Integer, Character> labels = new HashMap<>();
-    public int[] solution(int[][] edges) {        
+    Map<Integer, Integer> in = new HashMap<>();
+    Map<Integer, Integer> out = new HashMap<>();
+    Set<Integer> nums = new HashSet<>();
+    public int[] solution(int[][] edges) {
+        int[] answer = new int[4];
+        
         for (int[] edge: edges) {
-            if (!graph.containsKey(edge[0])) {
-                graph.put(edge[0], new LinkedList<>());
-            }
-            vs.add(edge[0]);
-            vs.add(edge[1]);
-            graph.get(edge[0]).add(edge[1]);
+            out.put(edge[0], out.getOrDefault(edge[0], 0) + 1);
+            in.put(edge[1], in.getOrDefault(edge[1], 0) + 1);
+            
+            nums.add(edge[0]);
+            nums.add(edge[1]);
         }
         
-        for (int v: vs) {
-            if (isStartOfDonut(v)) {
-                continue; 
-            }  
+        for (int num: nums) {
+            int inCnt = in.getOrDefault(num, 0);
+            int outCnt = out.getOrDefault(num, 0);
             
-            if (isStartOfStick(v)) {
-                 continue; 
+            if (inCnt == 0 && outCnt >= 2) {
+                answer[0] = num;
             }
             
-            isStartOfEight(v);
-        }
-        
-        
-        for (int v: vs) {
-            if (!labels.containsKey(v)) {
-                int[] answer = new int[]{v, 0, 0, 0};
-                for (int next: graph.get(v)) {
-                    if (labels.get(next) == 'd') answer[1]++;
-                    else if (labels.get(next) == 's') answer[2]++;
-                    else answer[3]++;
-                }
-                return answer;
-            }  
-        }
-        
-        return new int[4];
-    }
-                 
-    boolean isStartOfDonut(int v) {
-        int next = v;
-        while (true) {
-            q.add(next);
-            if (!graph.containsKey(next)) break;
-            
-            List<Integer> childs = graph.get(next);
-            if (childs.size() != 1) break;
-            
-            next = childs.get(0);
-            if (next == v) {
-                addLabel('d');
-                return true;
-            }
-        }
-        
-        q.clear();
-        return false;
-    }
-    
-    boolean isStartOfStick(int v) {
-        int next = v;
-        while (true) {
-            q.add(next);
-            if (!graph.containsKey(next)) {
-                addLabel('s');
-                return true;
+            else if (inCnt >= 1 && outCnt == 0) {
+                answer[2]++;
             }
             
-            List<Integer> childs = graph.get(next);
-            if (childs.size() != 1 || childs.get(0) == next) break;
-            
-            next = childs.get(0);            
-        }
-        
-        q.clear();
-        return false;
-    }
-    
-    boolean isStartOfEight(int v) {
-        if (!graph.containsKey(v)) return false;
-        
-        List<Integer> firstChilds = graph.get(v);
-        if (firstChilds.size() != 2) return false;
-        
-        q.add(v);
-        boolean isEight = true;
-        for (int c: firstChilds)  {
-            int next = c;
-            while (isEight) {
-                q.add(next);
-                
-                if (!graph.containsKey(next)) {
-                    isEight = false;
-                    break;
-                }
-                
-                List<Integer> childs = graph.get(next);
-                if (childs.size() != 1 && childs.get(0) != v) {
-                    isEight = false;
-                    break;
-                } 
-                
-                else if (childs.get(0) == v) {
-                    break;
-                }
-
-                next = childs.get(0);
+            else if (inCnt >= 2 && outCnt >= 2) {
+                answer[3]++;
             }
-        }
+        }    
         
-        if (isEight) addLabel('e');
-        q.clear();
-        return isEight;
-    }
-    
-    private void addLabel(char num) {
-        while (!q.isEmpty()) {
-            labels.put(q.poll(), num);
-        }
+        answer[1] = out.get(answer[0]) - (answer[2] + answer[3]);
+        return answer;
     }
 }
